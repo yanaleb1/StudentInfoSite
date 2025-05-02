cb463ff97026f34c6a668388416e0da3939a9c2c
yanaleb1 <leb.yana@ukr.net>
2025-05-03 01:28:31 +0300

    Початковий коміт з проєктом

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentInfoSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
